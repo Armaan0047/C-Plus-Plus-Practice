dfb162dddaf291b9e6c2b1ec19df0f83a2724c86
Mohd Armaan Tak <135983922+Armaan0047@users.noreply.github.com>
2026-03-29 23:34:49 +0530

    Create 12_prime_function.cpp

---
+++ Functions/12_prime_function.cpp
@@ -0,0 +1,27 @@
+// Program to check whether a number is prime using function
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if(n <= 1) return false;
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(isPrime(num))
+        cout << "Prime Number";
+    else
+        cout << "Not Prime";
+
+    return 0;
+}
